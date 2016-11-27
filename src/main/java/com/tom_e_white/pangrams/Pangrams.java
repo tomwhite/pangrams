5576f2ca5b32690add09fda7aa670c1b35e797f5
Tom White <tom@cloudera.com>
2016-11-27 20:35:57 +0000

    Column totals

--- src/main/java/com/tom_e_white/pangrams/Pangrams.java
+++ src/main/java/com/tom_e_white/pangrams/Pangrams.java
@@ -1,6 +1,7 @@
 package com.tom_e_white.pangrams;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Pangrams {
 
@@ -20,20 +21,27 @@ public class Pangrams {
   }
 
   public static int[] add(int[] p1, int[] p2) {
-    int[] p = new int[SIZE];
     for (int i = 0; i < SIZE; i++) {
-      p[i] = p1[i] + p2[i];
+      p1[i] = p1[i] + p2[i];
     }
-    return p;
+    return p1;
   }
 
   public static int[] minus(int[] p1, int[] p2) {
-    int[] p = new int[SIZE];
     for (int i = 0; i < SIZE; i++) {
-      p[i] = p1[i] - p2[i];
+      p1[i] = p1[i] - p2[i];
     }
-    return p;
+    return p1;
   }
+
+  public static int[] copy(int[] p) {
+    int[] copy = new int[SIZE];
+    for (int i = 0; i < SIZE; i++) {
+      copy[i] = p[i];
+    }
+    return copy;
+  }
+
 
   public static int[] profile(String s) {
     int[] p = new int[SIZE];
@@ -42,6 +50,34 @@ public class Pangrams {
       if (i >= 0) {
         p[i]++;
       }
+    }
+    return p;
+  }
+
+  public static final int[][] PROFILES = computeProfiles();
+
+  private static int[][] computeProfiles() {
+    String[] numbers = {
+      "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+        "seventeen", "eighteen", "nineteen",
+        "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four",
+        "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine",
+        "thirty", "thirty-one", "thirty-two", "thirty-three", "thirty-four",
+        "thirty-five", "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine",
+        "forty", "forty-one", "forty-two", "forty-three", "forty-four",
+        "forty-five", "forty-six", "forty-seven", "forty-eight", "forty-nine",
+    };
+    return Arrays.stream(numbers)
+        .map(Pangrams::profile)
+        .collect(Collectors.toList())
+        .toArray(new int[0][0]);
+  }
+
+  public static int[] columnTotals(int[] rows, int[] additionalLetters) {
+    int[] p = copy(additionalLetters);
+    for (int r : rows) {
+      add(p, PROFILES[r]);
     }
     return p;
   }
--- src/test/java/com/tom_e_white/pangrams/PangramsTest.java
+++ src/test/java/com/tom_e_white/pangrams/PangramsTest.java
@@ -13,4 +13,21 @@ public class PangramsTest {
     assertArrayEquals(new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0}, Pangrams.profile("two"));
     assertArrayEquals(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, Pangrams.profile("abcz"));
   }
+
+  @Test
+  public void testProfiles() {
+    assertArrayEquals(Pangrams.profile("zero"), Pangrams.PROFILES[0]);
+    assertArrayEquals(Pangrams.profile("one"), Pangrams.PROFILES[1]);
+    assertArrayEquals(Pangrams.profile("two"), Pangrams.PROFILES[2]);
+    assertArrayEquals(Pangrams.profile("three"), Pangrams.PROFILES[3]);
+  }
+
+  @Test
+  public void testColumnTotals() {
+    // numbers are from Figure 72
+    int[] rows =   { 27, 6, 3, 5, 11, 2, 20, 14, 6, 28, 29, 3, 6, 10, 4, 5 };
+    int[] extra =  { 7, 2, 2, 2, 4, 1, 10, 11, 2, 24, 7, 1, 2, 5, 1, 1};
+    int[] totals = { 27, 6, 3, 5, 11, 2, 20, 14, 6, 28, 21, 3, 6, 10, 4, 5 };
+    assertArrayEquals(totals, Pangrams.columnTotals(rows, extra));
+  }
 }
