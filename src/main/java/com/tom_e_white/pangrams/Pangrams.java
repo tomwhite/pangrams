d3e58056b7b6ad665c0feb5d706d09bf29391e0a
Tom White <tom@cloudera.com>
2016-11-27 20:00:45 +0000

    Add methods for equals, add, minus for fixed length int arrays.

--- src/main/java/com/tom_e_white/pangrams/Pangrams.java
+++ src/main/java/com/tom_e_white/pangrams/Pangrams.java
@@ -8,8 +8,35 @@ public class Pangrams {
     'e', 'f', 'g', 'h', 'i', 'l', 'n', 'o', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y'
   };
 
+  private static final int SIZE = PROFILE_LETTERS.length;
+
+  public static boolean equals(int[] p1, int[] p2) {
+    for (int i = 0; i < SIZE; i++) {
+      if (p1[i] != p2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static int[] add(int[] p1, int[] p2) {
+    int[] p = new int[SIZE];
+    for (int i = 0; i < SIZE; i++) {
+      p[i] = p1[i] + p2[i];
+    }
+    return p;
+  }
+
+  public static int[] minus(int[] p1, int[] p2) {
+    int[] p = new int[SIZE];
+    for (int i = 0; i < SIZE; i++) {
+      p[i] = p1[i] - p2[i];
+    }
+    return p;
+  }
+
   public static int[] profile(String s) {
-    int[] p = new int[16];
+    int[] p = new int[SIZE];
     for (char c : s.toCharArray()) {
       int i = Arrays.binarySearch(PROFILE_LETTERS, Character.toLowerCase(c));
       if (i >= 0) {
