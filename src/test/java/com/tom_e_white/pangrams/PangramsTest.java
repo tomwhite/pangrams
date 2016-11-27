456e6c7e864ad182495f263cee3ec19ee26ec412
Tom White <tom@cloudera.com>
2016-11-27 20:59:11 +0000

    More tests from the book.

--- src/test/java/com/tom_e_white/pangrams/PangramsTest.java
+++ src/test/java/com/tom_e_white/pangrams/PangramsTest.java
@@ -23,6 +23,18 @@ public class PangramsTest {
   }
 
   @Test
+  public void testAdditionalLetters() {
+    // from p208
+    String pangramTemplate =
+        "This pangram contains five a's, one b, two c's, two d's, ? e's, ? f's, ? g's, " +
+            "? h's, ? i's, one j, one k, ? l's, two m's, ? n's, ? o's, two p's, one q, " +
+            "? r's, ? s's, ? t's, ? u's, ? v's, ? w's, ? x's, ? y's, and one z.";
+    // numbers are from Figure 72
+    int[] extra =  { 7, 2, 2, 2, 4, 1, 10, 11, 2, 24, 7, 1, 2, 5, 1, 1};
+    assertArrayEquals(extra, Pangrams.profile(pangramTemplate));
+  }
+
+  @Test
   public void testColumnTotals() {
     // numbers are from Figure 72
     int[] rows =   { 27, 6, 3, 5, 11, 2, 20, 14, 6, 28, 29, 3, 6, 10, 4, 5 };
@@ -30,4 +42,19 @@ public class PangramsTest {
     int[] totals = { 27, 6, 3, 5, 11, 2, 20, 14, 6, 28, 21, 3, 6, 10, 4, 5 };
     assertArrayEquals(totals, Pangrams.columnTotals(rows, extra));
   }
+
+  @Test
+  public void testColumnTotalsMatch() {
+    // from p209, subtly different to the one on p208, since 'and' has become '&'
+    String pangramTemplate =
+        "This pangram contains four a's, one b, two c's, one d, ? e's, ? f's, ? g's, " +
+            "? h's, ? i's, one j, one k, ? l's, two m's, ? n's, ? o's, two p's, one q, " +
+            "? r's, ? s's, ? t's, ? u's, ? v's, ? w's, ? x's, ? y's, & one z.";
+
+    int[] rows =   { 30, 6, 5, 7, 11, 2, 18, 15, 5, 27, 18, 2, 7, 8, 2, 3 };
+    int[] extra =  Pangrams.profile(pangramTemplate);
+    int[] totals = rows; // since it is a pangram
+    assertArrayEquals(totals, Pangrams.columnTotals(rows, extra));
+  }
+
 }
