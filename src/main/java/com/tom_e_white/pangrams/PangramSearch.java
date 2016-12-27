253b6ec989426d2618403212f7606b6c4af214d5
Tom White <tom@cloudera.com>
2016-12-27 22:50:26 +0000

    Searches

--- src/main/java/com/tom_e_white/pangrams/PangramSearch.java
+++ src/main/java/com/tom_e_white/pangrams/PangramSearch.java
@@ -2,34 +2,50 @@ package com.tom_e_white.pangrams;
 
 public class PangramSearch {
   public static void main(String[] args) {
-    String name = "Tom";
-    String[] prologues = {
-        String.format("This pangram for %s contains", name),
-        String.format("This pangram for %s lists", name),
-        String.format("This pangram for %s includes", name),
-        String.format("This pangram for %s has", name),
-        String.format("This pangram for %s uses", name),
+    String[] names = {
+        "Lottie White",
+        "Emilia White",
+        "Eliane Wigzell",
+        "Tom White",
+        "Lottie",
+        "Emilia",
+        "Millie",
+        "Eliane",
+        "Tom",
     };
-    String[] connectives = {
-        "and", "&"
-    };
-    for (String prologue : prologues) {
-      for (String connective: connectives) {
-        String pseudoPangram = Pangrams.createPseudoPangram(prologue, connective);
-        SearchParameters search = Pangrams.getSearch(pseudoPangram);
-        System.out.println("Searching for '" + prologue + "' ... '" + connective + "' ...");
-        int[] profileCounts = Pangrams.search(search);
-        if (profileCounts == null) {
-          System.out.println("No pangram found");
-        } else {
-          String pangram = Pangrams.substituteProfile(pseudoPangram, profileCounts);
-          if (Pangrams.isPerfectPangram(pangram)) {
-            System.out.println("Eureka!");
-            System.out.println(pangram);
+    for (String name : names) {
+      String[] prologues = {
+          String.format("This pangram for %s contains", name),
+          String.format("This pangram for %s lists", name),
+          String.format("This pangram for %s includes", name),
+          String.format("This pangram for %s has", name),
+          String.format("This pangram for %s uses", name),
+          String.format("This pangram for %s features", name),
+          String.format("This pangram for %s shows", name),
+          String.format("This pangram for %s totals", name),
+          String.format("This pangram for %s enumerates", name),
+      };
+      String[] connectives = {
+          "and", "&"
+      };
+      for (String prologue : prologues) {
+        for (String connective: connectives) {
+          String pseudoPangram = Pangrams.createPseudoPangram(prologue, connective);
+          SearchParameters search = Pangrams.getSearch(pseudoPangram);
+          System.out.println("Searching for '" + prologue + "' ... '" + connective + "' ...");
+          int[] profileCounts = Pangrams.search(search);
+          if (profileCounts == null) {
+            System.out.println("No pangram found");
           } else {
-            System.out.println("Something went wrong, found");
-            System.out.println(pangram);
-            System.out.println("But it isn't a pangram...");
+            String pangram = Pangrams.substituteProfile(pseudoPangram, profileCounts);
+            if (Pangrams.isPerfectPangram(pangram)) {
+              System.out.println("Eureka!");
+              System.out.println(pangram);
+            } else {
+              System.out.println("Something went wrong, found");
+              System.out.println(pangram);
+              System.out.println("But it isn't a pangram...");
+            }
           }
         }
       }
